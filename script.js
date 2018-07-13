ae53fe81783c12a819892bb6bf932982ddf435ef
Cheri Allen <cherimarie@gmail.com>
2018-07-12 19:00:58 -0700

    Good click counting

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-// 1. get the element we want (maybe document.getElementById?)
-// 2. elVariableName.click syntax
-//    or addEventListener syntax (see debugging project for example)
+// 1. get the element we want, attach listener
+let clickable = document.querySelector("img.clickable")
+
+clickable.addEventListener("click", clickCounter)
 
 // declare the counter variable
 let counter = 0
 console.log("counter: ", counter)
 
-
 // click handler function
-
+function clickCounter(){
   // track total clicks- increment the counter variable
+  // counter = counter + 1
+  counter++
 
   // output click count to container in DOM
+  console.log("counter", counter)
+
+  // get the container, update its contents
+  let container = document.querySelector("p#clickcount")
+  container.innerHTML = counter
+}
